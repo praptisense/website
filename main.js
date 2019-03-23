45c9c8a813f97bb75300465a240607e74ca7d0a0
mohanen <bmohanen@gmail.com>
2019-03-23 11:53:34 +0530

    Font Change and Youtube video change

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
     $('#watchYtVideo').click(function () {
         var src = 'https://www.youtube.com/watch?v=obKXrYdTm1I&amp;autoplay=1';
         $('#ytModal').modal('show');
-        $('#ytModal .modal-body').html('<iframe class="shadow" width="100%" height="400" src="https://www.youtube.com/embed/obKXrYdTm1I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+        $('#ytModal .modal-body').html('<iframe class="shadow" width="100%" height="432" src="https://www.youtube.com/embed/Yro9ZpytyLs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
         $('#ytModal').modal('handleUpdate')
     });
 
